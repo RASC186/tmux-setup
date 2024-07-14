6647ed2342181f31433963d6341adfa21e38ae5c
Ramon Andrade Sousa Carvalho <ramon.andrade@live.com>
2024-07-14 13:38:34 -0300

    Add error checking

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+echo -e "\n\033[32mtmux Installer\033[0m\n"
+
+if sudo apt install -y tmux ; then
+
+  mkdir -p "${HOME}/.config/tmux/plugins/"
+
+  cp "./tmux.conf" "${HOME}/.config/tmux/"
+
+  git clone "https://github.com/tmux-plugins/tpm.git" \
+    "${HOME}/.config/tmux/plugins/tpm/"
+
+else
+  
+  echo -e "tmux installation failed."
+
+fi
